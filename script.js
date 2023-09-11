d2be9c1584ebb2ea7e74d2886acb26129273146c
alainsopp <131158946+alainsopp@users.noreply.github.com>
2023-09-11 10:27:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,267 @@
+function main () {
+    let minerList = `I;Americana 1-777;33000
+VI;Unreal Helipop;9787260
+I;The Primordial;8400000
+IV;Epic Tamborim;8175930
+III;Rare Singularity;6890625
+I;MINERVERSARY III;2700000
+II;Uncommon RollerMillion;2625000
+II;Uncommon Crypto Combo;1777965
+V;Legendary Disco-Roll;1724940
+II;Uncommon Minerzilla;1505550
+I;Pepperoni 10k;1500000
+V;Legendary Blaize;1235115
+I;Pencil-ReRoller;1136000
+III;Rare Vinylla 78-RPM;1051260
+I;Queen of Spin;862100
+III;Rare Anti-Phaze;861420
+III;Rare El Monstro;757995
+III;Rare JPEG Collectooor;689115
+I;The Big Top;640000
+I;Leviathan Clawster;618000
+I;Power Patriot;612000
+II;Uncommon Wavestorm;551250
+II;Uncommon Windwalk;551250
+I;Clover-n’-Over;530000
+I;Ancient;500000
+I;RollerFuture;500000
+IV;Epic The Savantron;488460
+I;Tamborim;452000
+I;The Nemesis;450000
+III;Rare Rust Runner;445200
+II;Uncommon The Golden Ticket;426615
+I;Tape-Side-B;418000
+III;Rare O Mineiro;413490
+I;The Christmas Eve;400000
+I;Perfection;400000
+II;Uncommon Lifebuoy;393750
+IV;Epic Hasher’s Egg;390915
+I;Steamwheedle;360000
+I;Paradox;350000
+III;Rare Dragon's Breath;344610
+I;Hand of the blockchain;331000
+IV;Epic Jungle King;304080
+I;Unicorn Dancer;303600
+I;Merry-go-Round;300000
+II;Uncommon El Monstro;288750
+III;Rare Crimson Bloom;275625
+I;Polar Express;270000
+II;Uncommon Jack-o'-Miner;262290
+I;Not-A-Loose-Cannon;262000
+I;Mascarado;252000
+I;Gandhi's Splittie;231600
+III;Rare Mission Control 321;227430
+I;Blaze-Boosted;213600
+I;Starground;210000
+I;Earthbind;210000
+III;Rare Blobpat;206745
+I;青火龙 (Azure Dragon);200000
+I;Air Mattress;200000
+I;The Minercracker;199500
+I;DOGER 420;199000
+II;Uncommon Survivor;196875
+I;Night’s Shade;189000
+I;M-Engine;188000
+I;Tape-Side-A;188000
+IV;Epic Candy Box;181125
+I;Think-Tronik;180000
+I;Heartbeat;176000
+III;Rare Octominer;172305
+II;Uncommon Rust Runner;169575
+II;Uncommon Cerberus;165375
+I;8bubbled;165000
+I;The Golden Ticket;162500
+I;Santa Sleigh;160000
+II;Uncommon O Mineiro;157500
+III;Rare Elven Magic;157185
+III;Rare White Christmas;157185
+I;Clover Lover;155000
+I;Gear Grabber;152550
+II;Vinylla 78-RPM;152550
+III;Rare Shrumee;151620
+I;Apollo Miner;147000
+I;Chupacabra;14000
+I;Prisma;140000
+III;Rare Chattanooga Choo;139545
+I;Chattanooga Choo;20230
+III;Rare Khepri;137865
+III;Rare TurtleCoin;137865
+III;Rare Mictlan;137865
+II;Uncommon Acropolis;131250
+II;Uncommon Uniminer-42;131250
+II;Uncommon Unity;131250
+II;Uncommon Flamin-Go;131250
+I;Minellite-130;130000
+II;Uncommon Gas Town Bitminer;126000
+I;Granny’s Cake;125000
+I;Afterparty;125000
+I;Anti-Phaze;125000
+I;Anti–Phaze;125000
+I;Photon;125000
+II;Uncommon Blaze Dragon;123900
+IV;Epic Rollerminer 761;118020
+I;Twin-Tire Driller;114000
+I;Shaolin Temple;112850
+III;Rare ToxiFilter 99;110250
+I;El Monstro;110000
+II;Uncommon Shifter;105000
+II;Uncommon Golden Corral;105000
+II;Uncommon Ouroboros;105000
+I;Prescilla;100000
+I;Nucleus;100000
+I;Eviltopus;100000
+II;Uncommon TYPE-Z;99750
+III;Rare Chupacabra;96495
+II;Uncommon Smokin' Hotrod;95340
+I;Smokin' Hotrod;36300
+I;Retro Night;95000
+I;Underwater;95000
+I;Sunset City;95000
+I;Thunderdome Tinkerer;92400
+II;Uncommon Armageddon;91875
+I;RollerShow;90000
+I;JBS-244;83100
+I;Happy Easter;80000
+I;Richie's Castle;80000
+I;Dancing Queen;78000
+I;Dream Demolisher 3000;70000
+IV;Epic Mergedge;69510
+III;Rare Purple Snowflake;68985
+II;Uncommon Octominer;65625
+I;Sandy's Castle;65000
+I;Ramesses II;65000
+I;Dawn-to-Dusk;64800
+I;Rust Runner;64600
+I;The Krank;60000
+I;O Mineiro;59970
+II;Uncommon Elven Magic;59850
+II;Uncommon White Christmas;59850
+II;Uncommon Shrumee;57750
+II;Uncommon Chattanooga Choo;53130
+II;Uncommon Fortune’s Charm;52500
+I;Fortune’s Charm;20000
+II;Uncommon The Leyak;52500
+II;Uncommon Rollerfury B8;50715
+I;Mine’n’Bass;50000
+I;King of Beasts ;50000
+I;Flamin-Go;50000
+I;The Black Pearl;50000
+I;Blaze Dragon;47200
+II;Uncommon Radio Gaga;40425
+I;Radio Gaga;15400
+I;Crimson Bloom;40000
+I;Velociminer;38750
+I;TYPE-Z;38000
+I;Disco-Roll;36300
+I;Toxic Crusader;35400
+I;Armageddon;35000
+I;Mycelium;35000
+I;Helltaurus;34000
+I;CP-106;11106
+I;8blessed;33000
+II;Uncommon Deepdiver;32865
+I;Deepdiver;12500
+I;Serenity;30000
+III;Rare Mergedge;26460
+I;Nuc;26000
+I;Milly Mini;25000
+I;Petroglyph;25000
+I;Sweet Stealer;23200
+I;Ghostly Pumper;22800
+I;Merry Dancers;22400
+I;Fury Fueler;22400
+I;Death Proof Digger;22000
+I;Shrumee;22000
+I;Entminer;22000
+I;Hasher’s Egg;21600
+I;Crimson Spark;20000
+I;Wishing Bell;20000
+I;Rollerfury B8;19300
+I;RPMiner;15400
+I;Subminerine;15000
+I;Gold Digger;15000
+I;JBS-200;13000
+I;The Thrill Rider;13000
+II;Uncommon Mergedge;10000
+I;Scarabey;8000
+I;RollerMiner S5+;4000
+I;Mergedge;3825
+I;RollerArc SX;3000
+I;RollerMiner S7;1320
+I;RollerMiner S4;1160
+III;Rare RollerArc S1;1155
+I;RollerArc SX;1000
+II;Uncommon Think-Tronik;472
+I;RollerArc S1;450
+II;Uncommon RollerArc S1;420
+III;Rare Raver Crab;315
+I;RollerArc S1;150
+I;Turkey Aid 2023;100
+I;Power Symbol;100
+I;Doggie-Woogie;10
+I;Demon Speeding;10
+I;Leap, The Frogo;10
+I;Raver Crab;1
+II;Uncommon Mergedge Mk. IV;17010
+I;Mergedge Mk. IV;6450
+III;Rare Rolleron 741;19110
+II;Uncommon Rolleron 741;7245
+I;Rolleron 741;2730
+II;Uncommon RollerMiner G1;9240
+II;Uncommon RollerMiner R4;15750
+II;Rare RollerMiner S4;8085
+II;Rare RollerMiner S7;9135
+IV;Epic RollerArc S1;3045
+I;Mergedge Mk. I;4860
+I;RollerMiner G1;3500
+II;Uncommon RollerMiner S4;3045
+I;Candy Box;10000
+II;Uncommon RollerMiner S7;3465
+I;RollerMiner R4;6000
+II;Uncommon Mergedge Mk. I;12810
+I;Drifter;24000
+III;Rare Mergedge Mk. I;33705`.split('\n');
+    getbestRatioMiner(minerList);
+}
+
+function getbestRatioMiner(minerList) {
+    let names = [];
+    let unlistedMiners = [];
+    let prices = [];
+    let bestRatioMiner = { 'Name' : '', 'Power' : 0, 'Price' : 0, 'Ratio' : 0 };
+    let minerNames = document.getElementsByClassName('item-title');
+    for (let i = 0; i < minerNames.length - 1; i++) {
+        names.push(minerNames[i].textContent)
+    }
+    let minerPrices = document.getElementsByClassName('item-price');
+    for (let i = 0; i < minerPrices.length - 1; i++) {
+        prices.push(minerPrices[i].textContent.split(" ")[0])
+    }
+    if (names.length == prices.length) {
+        for (let i = 0; i < names.length - 1; i++) {
+            miner = minerList.filter(e => e.split(';')[1] == names[i]);
+            if (miner.length != 0) {
+                let splited = miner[0].split(';');                
+                let ratio = (parseInt(splited[2]) / 1000) / parseFloat(prices[i]);                
+                if (bestRatioMiner.Ratio < ratio) {
+                    bestRatioMiner.Name = splited[1];
+                    bestRatioMiner.Power = splited[2];
+                    bestRatioMiner.Price = prices[i];
+                    bestRatioMiner.Ratio = ratio
+                }
+            } else {
+                unlistedMiners.push(names[i])
+            }
+        }
+    }
+    console.log(bestRatioMiner);
+    if (unlistedMiners.length == 0) {
+        console.log("[ All miners on a page were analyzed.]")
+    }
+    else {
+        console.log("Unlisted Miners: ")
+        console.log(unlistedMiners)
+    }
+}
+
+main();
