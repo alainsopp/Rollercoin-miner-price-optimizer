bcb461fd273ef246126c6a2cd5be1175f3fa1343
kutor <kutor@p3x5456.home>
2023-11-11 17:52:02 +0100

    Adds new miners

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function main () {
-    let minerList = `I;Americana 1-777;33000
-VI;Unreal Helipop;9787260
+    let minerList = `VI;Unreal Helipop;9787260
 I;The Primordial;8400000
 IV;Epic Tamborim;8175930
 III;Rare Singularity;6890625
 I;MINERVERSARY III;2700000
+II;Uncommon MINERVERSARY III;2362500
 II;Uncommon RollerMillion;2625000
 II;Uncommon Crypto Combo;1777965
 V;Legendary Disco-Roll;1724940
@@ -42,6 +42,7 @@ III;Rare Dragon's Breath;344610
 I;Hand of the blockchain;331000
 IV;Epic Jungle King;304080
 I;Unicorn Dancer;303600
+IV;Epic Unicorn Dancer;5491605
 I;Merry-go-Round;300000
 II;Uncommon El Monstro;288750
 III;Rare Crimson Bloom;275625
@@ -54,11 +55,13 @@ III;Rare Mission Control 321;227430
 I;Blaze-Boosted;213600
 I;Starground;210000
 I;Earthbind;210000
+V;Legendary Earthbind;9971640
 III;Rare Blobpat;206745
 I;青火龙 (Azure Dragon);200000
 I;Air Mattress;200000
 I;The Minercracker;199500
 I;DOGER 420;199000
+I;Survivor;75000
 II;Uncommon Survivor;196875
 I;Night’s Shade;189000
 I;M-Engine;188000
@@ -77,22 +80,23 @@ III;Rare Elven Magic;157185
 III;Rare White Christmas;157185
 I;Clover Lover;155000
 I;Gear Grabber;152550
-II;Vinylla 78-RPM;152550
+I;Vinylla 78-RPM;152550
 III;Rare Shrumee;151620
 I;Apollo Miner;147000
-I;Chupacabra;14000
+I;Chupacabra;140000
 I;Prisma;140000
 III;Rare Chattanooga Choo;139545
-I;Chattanooga Choo;20230
 III;Rare Khepri;137865
+I;TurtleCoin;20000
+II;Uncommon TurtleCoin;52500
 III;Rare TurtleCoin;137865
+V;Legendary TurtleCoin;950145
 III;Rare Mictlan;137865
 II;Uncommon Acropolis;131250
 II;Uncommon Uniminer-42;131250
 II;Uncommon Unity;131250
 II;Uncommon Flamin-Go;131250
 I;Minellite-130;130000
-II;Uncommon Gas Town Bitminer;126000
 I;Granny’s Cake;125000
 I;Afterparty;125000
 I;Anti-Phaze;125000
@@ -113,7 +117,6 @@ I;Eviltopus;100000
 II;Uncommon TYPE-Z;99750
 III;Rare Chupacabra;96495
 II;Uncommon Smokin' Hotrod;95340
-I;Smokin' Hotrod;36300
 I;Retro Night;95000
 I;Underwater;95000
 I;Sunset City;95000
@@ -134,12 +137,15 @@ I;Dawn-to-Dusk;64800
 I;Rust Runner;64600
 I;The Krank;60000
 I;O Mineiro;59970
+I;Elven Magic;22800
 II;Uncommon Elven Magic;59850
+V;Legendary Elven Magic;1083285
 II;Uncommon White Christmas;59850
 II;Uncommon Shrumee;57750
+I;Chattanooga Choo;20230
 II;Uncommon Chattanooga Choo;53130
 II;Uncommon Fortune’s Charm;52500
-I;Fortune’s Charm;20000
+II;Uncommon Fortune’s Charm;52500
 II;Uncommon The Leyak;52500
 II;Uncommon Rollerfury B8;50715
 I;Mine’n’Bass;50000
@@ -148,7 +154,6 @@ I;Flamin-Go;50000
 I;The Black Pearl;50000
 I;Blaze Dragon;47200
 II;Uncommon Radio Gaga;40425
-I;Radio Gaga;15400
 I;Crimson Bloom;40000
 I;Velociminer;38750
 I;TYPE-Z;38000
@@ -160,7 +165,6 @@ I;Helltaurus;34000
 I;CP-106;11106
 I;8blessed;33000
 II;Uncommon Deepdiver;32865
-I;Deepdiver;12500
 I;Serenity;30000
 III;Rare Mergedge;26460
 I;Nuc;26000
@@ -173,6 +177,7 @@ I;Fury Fueler;22400
 I;Death Proof Digger;22000
 I;Shrumee;22000
 I;Entminer;22000
+VI;Unreal Entminer;2743020
 I;Hasher’s Egg;21600
 I;Crimson Spark;20000
 I;Wishing Bell;20000
@@ -184,43 +189,158 @@ I;JBS-200;13000
 I;The Thrill Rider;13000
 II;Uncommon Mergedge;10000
 I;Scarabey;8000
+II;Uncommon Scarabey;21000
+III;Rare Scarabey;55125
+I;RollerMiner S5+;12000
 I;RollerMiner S5+;4000
-I;Mergedge;3825
+II;Uncommon RollerMiner S5+;10500
+III;Rare RollerMiner S5+;27615
+IV;Epic RollerMiner S5+;72555
+V;Legendary RollerMiner S5+;190470
+VI;Unreal RollerMiner S5+;500010
+I;Mergeded;3825
 I;RollerArc SX;3000
 I;RollerMiner S7;1320
+II;Uncommon RollerMiner S7;3465
 I;RollerMiner S4;1160
+I;RollerArc S1;450
+I;RollerArc S1;150
+II;Uncommon RollerArc S1;420
 III;Rare RollerArc S1;1155
+IV;Epic RollerArc S1;3045
+V;Legendary RollerArc S1;8085
 I;RollerArc SX;1000
 II;Uncommon Think-Tronik;472
-I;RollerArc S1;450
-II;Uncommon RollerArc S1;420
 III;Rare Raver Crab;315
-I;RollerArc S1;150
 I;Turkey Aid 2023;100
 I;Power Symbol;100
 I;Doggie-Woogie;10
 I;Demon Speeding;10
 I;Leap, The Frogo;10
 I;Raver Crab;1
-II;Uncommon Mergedge Mk. IV;17010
-I;Mergedge Mk. IV;6450
-III;Rare Rolleron 741;19110
-II;Uncommon Rolleron 741;7245
-I;Rolleron 741;2730
-II;Uncommon RollerMiner G1;9240
-II;Uncommon RollerMiner R4;15750
-II;Rare RollerMiner S4;8085
-II;Rare RollerMiner S7;9135
-IV;Epic RollerArc S1;3045
+I;Smelt Smith;21000
+II;Uncommon Smelt Smith;55125
+III;Rare Smelt Smith;144795
+I;BioGauge 67;43800
+I;RadExFiltration 90;107500
+II;Uncommon RadExFiltration 90;282240
+II;Uncommon BioGauge 67;114975
+I;Milly Mini;75000
+I;It'sOkayer;282000
+II;Uncommon Hand of the Blockchain;868875
+I;The Creeper;90000
+I;Aurum;200000
+III;Rare Aurum;1378125
+I;Fire Forger;149000
+I;ToxiFilter 99;16000
+II;Uncommon Fire Forger;391125
+I;JPEG Collectooor;100000
+II;Uncommon JPEG Collectooor;262500
+II;Uncommon DOGER 420;522375
+II;Uncommon Tu Nian;890295
+I;The Savantron;27000
 I;Mergedge Mk. I;4860
+I;Mergedge;3825
+I;Rebirth Rover;44000
+II;Uncommon RollerMiner G1;9240
+I;TAPEBOY;12200
+II;Uncommon TAPEBOY;32025
+III;Rare TAPEBOY;84105
+V;Legendary TAPEBOY;579705
+I;TAPEBOY;36600
+II;Uncommon Roller Industries Co.;315000
+I;Roller Industries Co.;120000
+I;Sparkling Light;10000
+I;Paradise Pier;20020
+II;Uncommon Paradise Pier;52605
+III;Rare Paradise Pier;138180
+I;Gas Town Bitminer;48000
+II;Uncommon Gas Town Bitminer;126000
+II;Uncommon Dawn-To-Dusk;170100
+III;Rare Dawn-To-Dusk;446565
+I;Inclusion Miner;71400
+II;Uncommon Inclusion Miner;187425
+III;Rare Inclusion Miner;492030
+IV;Epic Inclusion Miner;1291605
+I;XLZ-01;22000
+II;Uncommon XLZ-01;57750
+III;Rare XLZ-01;151620
+I;Chaise Longue;25000
+II;Uncommon Chaise Longue;65625
+III;Rare Chaise Longue;172305
+I;Your Friend Turing;42000
+I;Target Flooff;56000
+II;Uncommon Target Flooff;147000
+III;Rare Target Flooff;385875
+I;Country Loading...;50500
+II;Uncommon Country Loading...;132615
+III;Rare Country Loading...;348180
+I;Katsuhito;10
+I;The Protector;36000
+III;Rare The Savantron;186060
+II;Uncommon The Savantron;70875
+I;Star Symbol;100
+I;Fang Symbol;100
+I;Whatta Duck;10
+I;Vegvisir;24000
+I;Missionaries;40000
+II;Uncommon Missionaries;105000
+III;Rare Missionaries;275625
+IV;Epic Missionaries;723555
+II;Uncommon Leap, The Frogo;105
+III;Rare Leap, The Frogo;315
+IV;Epic Leap, The Frogo;840
+VI;Unreal Leap, The Frogo;5880
 I;RollerMiner G1;3500
-II;Uncommon RollerMiner S4;3045
-I;Candy Box;10000
-II;Uncommon RollerMiner S7;3465
+V;Legendary RollerMiner G1;167370
+IV;Epic RollerMiner G1;63735
+III;Rare RollerMiner G1;24255
+I;Mission Control 321;33000
+I;Nuclearburg;35200
+I;Bit by Beat;32400
+I;Jungle King;16800
+I;Snowster-9001;35400
+I;Sweets Stealer;23200
+I;Gingerbread House;22000
+II;Uncommon Doggie-Woogie;105
+I;Black Cat;50000
+II;Uncommon Black Cat;131250
+III;Rare Black Cat;344610
+I;Awaken;195000
+II;Uncommon Awaken;511875
+I;Night Witnesses;240000
+II;Uncommon Night Witnesses;630000
+I;Heretic Opera Miner;580000
+I;Boogieman;870000
+II;Uncommon Boogieman;2283750
+I;Booo!;1150000
+II;Uncommon Booo!;3018750
+I;Pumpkin King;1915000
+I;Dreamweaver;80400
+I;Mega Maner;130500
+I;CommunityCatalyst;56400
+I;Pacminer;403000
+I;Devil's Carnival;870000
+I;YMCA Game;114000
+I;The Icarus;225000
+I;Hamster's Enemy;114000
+IV;Epic Guess-the-Card;19534935
+III;Rare CyberSun;5236875
+I;SkullMiner;7900000
+II;Uncommon RollerFuture;1312500
+I;JBS-144;83100
+IV;Epic Corn Popper;2532495
+V;Legendary Gandhi's Splittie;10142475
+I;Inferno Igniter;67200
+I;Emperor's Treasure;40000
+I;Isotope Miner 23;164300
+II;Uncommon Hasher’s Egg;56700
+I;Purity;87750
+I;Ouroboros;40000
+I;HauntedHouse;125000
 I;RollerMiner R4;6000
-II;Uncommon Mergedge Mk. I;12810
-I;Drifter;24000
-III;Rare Mergedge Mk. I;33705`.split('\n');
+I;Rolleron 741;2730
+I;Rolleron Miner 761;6500`.split('\n');
     miners = getBestRatioMiners(minerList);
     console.log(miners);
 }
